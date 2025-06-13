ff52fcc380de84f03debf27e6ba13a34cc3e8e13
amaddouri <atef.maddouri@be-softilys.tn>
2025-06-13 15:40:42 +0100

    working kafka

---
+++ car-service/src/main/java/com/carrefour/carservice/model/Customer.java
@@ -0,0 +1,19 @@
+package com.carrefour.carservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "customers")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Customer {
+    @Id
+    private String id;
+    private String username;
+}
---
+++ car-service/src/main/java/com/carrefour/eventData/EventType.java
@@ -0,0 +1,7 @@
+package com.carrefour.eventData;
+
+public enum EventType {
+    CREATED_CUSTOMER_EVENT,
+    UPDATED_CUSTOMER_EVENT,
+    DELETED_CUSTOMER_EVENT
+}
